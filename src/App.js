32c3939c8e09fd104ad07ead03d38357c8a27fa8
Michal Klauza <klauza.uk@gmail.com>
2020-05-23 16:58:26 +0100

    add Footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import Hero from './components/Hero';
 import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <Projects />
-
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Content } from './FooterCSS';
+
+const Footer = () => {
+  return (
+    <Content>
+      FOOTER
+    </Content>
+  )
+}
+
+export default Footer

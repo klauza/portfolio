38494b1d3ab41dd9c7711ab85c43c169407d48f9
Michal Klauza <klauza.uk@gmail.com>
2020-05-27 17:31:52 +0100

    push build

---
+++ build/precache-manifest.49895be52beb2f5e5b53dbcb54517383.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56e6666dc7bd2629107ab799a1d9ca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad5871f671237bccf657",
+    "url": "/static/css/main.7c6946d7.chunk.css"
+  },
+  {
+    "revision": "d1e5581c2ded166962f6",
+    "url": "/static/js/2.c5550a13.chunk.js"
+  },
+  {
+    "revision": "702e06c21f4db63e7e05a8a0eda9baf1",
+    "url": "/static/js/2.c5550a13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad5871f671237bccf657",
+    "url": "/static/js/main.f58a35da.chunk.js"
+  },
+  {
+    "revision": "94a1f49ea9db9d2e2d5b",
+    "url": "/static/js/runtime-main.d22a7409.js"
+  },
+  {
+    "revision": "e547a578794634635212f480b58e091d",
+    "url": "/static/media/footer_bg.e547a578.png"
+  },
+  {
+    "revision": "03d95881eed4db0f5c6f902164922deb",
+    "url": "/static/media/hero_bg.03d95881.png"
+  }
+]);
